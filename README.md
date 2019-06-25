dcce637b767161abfcba2187cedb0de995c1e7b5
Pedro Nahime <pedronahime@MacBook-Pro-de-Pedro.local>
2019-06-24 23:36:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Busca_gif_app
+
+App Simples para buscar gifs
+
+## Getting Started
+
+Crie uma key no site [giphy developers](https://developers.giphy.com/)
+e coloque na api no arquivo home_page.dart
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gif/ui/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(primaryColor: Colors.white, hintColor: Colors.white),
+      home: HomePage(),
+    ),
+  );
+}
